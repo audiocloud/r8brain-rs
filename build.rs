55bc5b86615776161074ca9311dacf2fa4df8a1e
Bojan Šernek <bojan@distopiksound.com>
2022-10-26 17:35:48 +0800

    add build type to the search path for MSVC cmake compatibility

--- build.rs
+++ build.rs
@@ -1,8 +1,15 @@
 use std::env;
 
 fn main() {
+    let build_type = if cfg!(feature = "debug") {
+        "Debug"
+    } else {
+        "Release"
+    };
+
     let dst = cmake::Config::new(".").build_target("r8brain").build();
 
+    println!("cargo:rustc-link-search=native={}/build/{}", dst.display(), build_type);
     println!("cargo:rustc-link-search=native={}/build", dst.display());
     println!("cargo:rustc-link-search=native={}", dst.display());
     println!("cargo:rustc-link-lib=static=r8brain");
