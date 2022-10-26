ca5243a2f53cd2a1436b93df3809f7ff81548dbe
Bojan Šernek <bojan@distopiksound.com>
2022-10-26 18:03:02 +0800

    debug is nut a feature, use env PROFILE instead

--- build.rs
+++ build.rs
@@ -1,17 +1,11 @@
 use std::env;
 
 fn main() {
-    let build_type = if cfg!(feature = "debug") {
-        "Debug"
-    } else {
-        "Release"
-    };
-
     let mut dst = cmake::Config::new(".").build_target("r8brain").build();
     println!("cargo:rustc-link-search=native={}", dst.display());
     dst.push("build");
     println!("cargo:rustc-link-search=native={}", dst.display());
-    dst.push(build_type);
+    dst.push(env::var("PROFILE").expect("Profile must be specified"));
     println!("cargo:rustc-link-search=native={}", dst.display());
     println!("cargo:rustc-link-lib=r8brain");
 
